a8bf40d8dd0df11cbbe48dbb03a987a40790363c
Lukas Pietzschmann <lukas.pietzschmann@uni-ulm.de>
2024-12-12 14:00:20 +0100

    Better formatting

--- R/mutations.R
+++ R/mutations.R
@@ -286,19 +286,13 @@ create_call <- list(
     return(is.call(ast) && ast[[1]] == "{")
   },
   get_mutations = function(ast) {
-    return(list(list(
-      id = "add warning",
-      fun = function() {
-        as <- c(as.list(ast[-1]), quote(warning("warning created by mutatr")))
-        return(as.call(c(ast[[1]], as)))
-      }
-    ), list(
-      id = "add error",
-      fun = function() {
-        as <- c(as.list(ast[-1]), quote(stop("error created by mutatr")))
-        return(as.call(c(ast[[1]], as)))
-      }
-    )))
+    return(list(list(id = "add warning", fun = function() {
+      as <- c(as.list(ast[-1]), quote(warning("warning created by mutatr")))
+      return(as.call(c(ast[[1]], as)))
+    }), list(id = "add error", fun = function() {
+      as <- c(as.list(ast[-1]), quote(stop("error created by mutatr")))
+      return(as.call(c(ast[[1]], as)))
+    })))
   }
 )
 
